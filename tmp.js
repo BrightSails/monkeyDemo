8df85009c7f88fbe0584cd5984b37b3f2e8001cb
BrightSails <2630594775@qq.com>
2022-10-11 17:11:44 +0800

    ptt

--- tmp.js
+++ tmp.js
@@ -8,10 +8,32 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
 // @grant        none
 // ==/UserScript==style="
-
 (function() {
     'use strict';
+    document.querySelectorAll('#outer')[0].innerHTML=""
+    document.querySelectorAll('#info_block > tbody > tr:nth-child(1) > td > div')[0].innerHTML=""
+    document.querySelectorAll('#ka65')[0].setAttribute('style','display:none')
 
-    console.log("second");
+    const randomColor = () => `#${Math.random().toString(16).substr(2, 6)}`;    
+
+    let a = document.querySelectorAll('#info_block > tbody > tr:nth-child(1) > td > span > a > b')[0].innerHTML;
+    let x= "";
+    for(let b = 0;b<5;b++){
+        x+="<font color=\""+ randomColor() +"\">" + a[b] + "</font>"
+    }
+    let p =document.querySelectorAll('#info_block > tbody > tr:nth-child(1) > td > span > a > b')[0]
+    
+    function showLogin()
+    {
+        let x= "";
+        for(let b = 0;b<5;b++){
+            x+="<font color=\""+ randomColor() +"\">" + a[b] + "</font>"
+        }
+        p.innerHTML = x
+    }
+    setInterval("showLogin()","1000");
+
     // Your code here...
 })();
+
+#info_block > tbody > tr:nth-child(3) > td > div
