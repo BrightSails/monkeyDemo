f4da0878a9d2ee737f8e44efa95bc08a0751ad8d
BrightSails <2630594775@qq.com>
2022-10-11 15:36:33 +0800

    3

---
+++ tmp.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         RainBow
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.pttime.org/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
+// @grant        none
+// ==/UserScript==style="color:#fff444"
+
+(function() {
+    'use strict';
+    corsole.log("second version");
+    // Your code here...
+})();
