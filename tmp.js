844cb60a8e0834198e48f40379131df00f0acab3
BrightSails <2630594775@qq.com>
2022-10-11 17:55:41 +0800

    ptt0.24

--- tmp.js
+++ tmp.js
@@ -4,7 +4,7 @@
 // @version      0.2
 // @description  color name!
 // @author       You
-// @match        https://www.pttime.org/*
+// @match        @include https://www.pttime.org/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
 // @grant        none
 // ==/UserScript==style="
