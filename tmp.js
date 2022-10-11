b48a0579a7a806243420171e5a6b034db11eeaec
BrightSails <2630594775@qq.com>
2022-10-11 18:31:31 +0800

    ptt0.3

--- tmp.js
+++ tmp.js
@@ -11,11 +11,6 @@
 
 (function() {
     'use strict';
-   // document.querySelectorAll('#outer')[0].innerHTML=""
-   // document.querySelectorAll('#info_block > tbody > tr:nth-child(3) > td > div')[0].innerHTML = ""
-   // document.querySelectorAll('#ka65')[0].setAttribute('style','display:none')
-   // document.querySelectorAll('#info_block > tbody > tr:nth-child(2) > td > div.left > span:nth-child(7) > font')[0].innerHTML = ""
-    
     const randomColor = () => `#${Math.random().toString(16).substr(2, 6)}`;    
     let a = document.querySelectorAll('#info_block > tbody > tr:nth-child(1) > td > span > a > b')[0].innerHTML;
     let x= "";
@@ -31,6 +26,13 @@
        }
        p.innerHTML = x
    }, 500);
+
+   document.querySelectorAll('#outer')[0].innerHTML=""
+   document.querySelectorAll('#info_block > tbody > tr:nth-child(3) > td > div')[0].innerHTML = ""
+   document.querySelectorAll('#ka65')[0].setAttribute('style','display:none')
+   document.querySelectorAll('#info_block > tbody > tr:nth-child(2) > td > div.left > span:nth-child(7) > font')[0].innerHTML = ""
+    
+    
    //window.open('https://www.pttime.org/attendance.php','_self');
   
 })();
